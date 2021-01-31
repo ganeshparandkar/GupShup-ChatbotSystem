43b673a35723b17c118a3ca59db123a276b1ce76
Shriyash Parandkar <41060734+ganeshparandkar@users.noreply.github.com>
2021-02-01 03:37:00 +0530

    working

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,7 @@ var database = admin.database();
 exports.apicall = functions.https.onRequest((req, res) => {
   const data = req.body.payload.payload;
   const userData = req.body.payload;
+  const inputdata = data.text;
 
   const startConvo = [
     'hi',
@@ -32,284 +33,72 @@ exports.apicall = functions.https.onRequest((req, res) => {
     Logical programming starts
 -------*/
   if (req.method == 'POST') {
-    // ????????????????????????????????
-    // database trial
-    // ---------------------------------------
-    // if (startConvo.includes(data.text)) {
-    // try {
-    //   database.ref(`chatbot/id/${userData.sender.phone}`).set({
-    //     id: 1,
-    //     name: `${userData.sender.name}`,
-    //   });
-    //   res.send('added');
-    // } catch (error) {
-    //   res.send(error.toString());
-    // }
-    // } else {
-    // try {
-    //  ?working
-    // var ref = database.ref(`chatbot/id/${userData.sender.phone}/id`);
-    // ref.on(
-    //   'value',
-    //   (snapshot) => {
-    //     mycount = snapshot.val();
-    //   },
-    //   (errorObject) => {
-    //     console.log('The read failed: ' + errorObject.code);
-    //   }
-    // );
-    // res.send(mycount.toString());
-    // } catch (error) {
-    //   res.send(error.toString());
-    // }
-    // ?update code working
-    // try {
-    //   database.ref(`chatbot/id/${userData.sender.phone}`).update({
-    //     id: 2,
-    //   });
-    //   res.send('updated');
-    // } catch (error) {
-    //   res.send(error.toString());
-    // }
-  }
+    // !
 
-  // ????????????????????????????????
-
-  // !
-
-  // if no. is exist then check the count if no. is not exist then create the count
-  // if (data.text == 'count') {
-  //   try {
-  //     var ref = database.ref(`chatbot/id/${userData.sender.phone}/count`);
-  //     ref.on(
-  //       'value',
-  //       (snapshot) => {
-  //         mycount = snapshot.val();
-  //       },
-  //       (errorObject) => {
-  //         console.log('The read failed: ' + errorObject.code);
-  //       }
-  //     );
-  //     console.log(mycount);
-  //     res.send(mycount);
-  //   } catch (error) {
-  //     res.send(error.toString());
-  //   }
-  // }
-
-  if (startConvo.includes(data.text)) {
-    try {
-      database.ref(`chatbot/id/${userData.sender.phone}`).set({
+    if (startConvo.includes(data.text)) {
+      database.ref(`chatbot/${userData.sender.phone}`).set({
         count: 1,
         name: `${userData.sender.name}`,
         phone: `${userData.sender.phone}`,
       });
       res.send('Hi \n Enter pincode');
-    } catch (error) {
-      res.send(error.toString());
     }
-  }
-  // read pincode
-  if (data.text.length === 6) {
-    pincode = data.text;
-    // save pincode to the databasse
-    try {
+
+    if (!isNaN(inputdata)) {
+      let cval;
+
       database
-        .ref(`chatbot/id/${userData.sender.phone}`)
-        .update({
-          count: 2,
-          pincode: `${pincode}`,
-        })
-        .then(
-          res.send(`Thanks, here is today's menu. Please let us know what you want, make sure you enter the item number and qty.\n
-      1 - Product 1 \n
-      2 - Product 2 \n
-      3 - Product 3\n
-      please enter number input`)
-        )
-        .catch((e) => {
-          res.send(e.toString());
+        .ref('chatbot')
+        .child(`${userData.sender.phone.toString()}`)
+        .on('value', (snapshot) => {
+          // console.log(typeof snapshot.val().id);
+          cval = snapshot.val().count;
         });
-    } catch (error) {
-      res.send(error.toString());
-    }
-  }
 
-  if (data.text.length > 3) {
-    var mycount;
-    
-    var ref = database.ref(`chatbot/id/${userData.sender.phone}/id`);
-    ref.on(
-      'value',
-      (snapshot) => {
-        mycount = snapshot.val();
-      },
-      (errorObject) => {
-        console.log('The read failed: ' + errorObject.code);
+      if (inputdata.length === 6) {
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('pincode')
+          .set(inputdata);
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('count')
+          .set(2);
+        res.send(
+          `pincode is set ${inputdata} \n now enter item no and value of count is ${cval} and type of cval is ${typeof cval}`
+        );
       }
-    );
-    res.send(mycount.toString());
 
-    mycount = parseInt(mycount.toString());
-    
-    if(mycount == 2){
-      // read for input and ask for quantity  
-      res.send('enter the quantity')
-    }
-    if (mycount == 3) {
-      // read for quaantity
-      res.send("enter the address")
+      if (inputdata.length < 3) {
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('count')
+          .set(3);
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('itemId')
+          .set(inputdata);
+        res.send(
+          `you have selected ${inputdata}  item.\n now enter item quantity`
+        );
+      }
+      if (cval == 3) {
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('count')
+          .set(4);
+        database
+          .ref('chatbot')
+          .child(userData.sender.phone)
+          .child('itemquantity')
+          .set(inputdata);
+        res.send(`you ordered ${inputdata} items.\n now enter the address`);
+      }
     }
   }
-
-  // !
-
-  // get the value of count variable
-
-  /* -----
- database fetching code 
--------*/
-  // var ref = database.ref(`chatbot/id/${userData.sender.phone}/count`);
-  // ref.on(
-  //   'value',
-  //   (snapshot) => {
-  //     xyz = snapshot.val();
-  //   },
-  //   function (errorObject) {
-  //     console.log('The read failed: ' + errorObject.code);
-  //   }
-  // );
-  // res.send(`this is the response ${xyz}`);
-
-  /* -----
- database setting done here
--------*/
-
-  // database.ref(`chatbot/id/${userData.sender.phone}`).set({
-  //   count: 5,
-  //   name: userData.sender.name,
-  //   phoneNumber: userData.sender.phone,
-  // });
-
-  // var result = 'dontknow';
-  // res.send('trying to connect the database wait for the message');
-  // if (req.data.text === 'hi') {
-  //   try {
-  //     res.send('started');
-  //     database.ref('chatbot/id').set({
-  //       count: 2,
-  //     });
-  //     result = 'success';
-  //     res.send(result);
-  //   } catch (error) {
-  //     res.send(error);
-  //   }
-  // }
-
-  // const pincode, itemId, itemQuantity, address;
-  // let qcount = 0;
-  // const data = req.body.payload.payload;
-
-  /* --------
-    1sst case
-     ----------*/
-  // switch (qcount) {
-  //   case 0:
-
-  // ?
-  // if (
-  //   data.text === 'hi' ||
-  //   data.text === 'Hi' ||
-  //   data.text === 'hii' ||
-  //   data.text === 'Hii'
-  // ) {
-  //   qcount = 1;
-  //   res.send(`${qcount} Hi, Please provide the PIN Code.`);
-  // }
-
-  // ?
-  //     break;
-  //   case 1:
-
-  //     break;
-  //   case 0:
-
-  //     break;
-  //   case 0:
-
-  //     break;
-  //   case 0:
-
-  //     break;
-
-  //   default:
-  //     break;
-  // }
-
-  /* --------
-    2nd case
-     ----------*/
-
-  // if (data.text.length === 6) {
-  //   qcount = 2;
-  //   pincode = data.text;
-  //   res.send(`${qcount} Thanks, here is today's menu. Please let us know what you want, make sure you enter the item number and qty.\n
-  //   1 - Product 1 \n
-  //   2 - Product 2 \n
-  //   3 - Product 3\n
-  //   please enter number input`);
-  // }
-  /* --------
-    3rd case
-     ----------*/
-  // if (data.text.length === 1) {
-  //   var checkinput = parseInt(data.text);
-  //   res.send(`Please enter quantity`);
-  // }
-  /* --------
-    4th case
-     ----------*/
-
-  //  user sends the quantity read that and ask for address
-
-  /* --------
-    5th case
-     ----------*/
-
-  //  user sends the qddress read that and send thank you response
-
-  // res.send(data.payload.sender.phone);
 });
-
-// ? for reference
-// ? when user sends message to the bot
-// const data = [
-//   {
-//     app: 'DevSquare',
-//     timestamp: 1611901109395,
-//     version: 2,
-//     type: 'message',
-//     payload: {
-//       id: 'ABEGkYQheTRnAhCrlA6beoBEc6hm4sGG5FDJ',
-//       source: '918421793467',
-//       type: 'text',
-//       payload: { text: 'Sorry 😂😂' },
-//       sender: {
-//         phone: '918421793467',
-//         name: 'Shriyash ✌🏻',
-//         country_code: '91',
-//         dial_code: '8421793467',
-//       },
-//     },
-//   },
-// ];
-
-// ? json
-// const userData = [
-//   {
-//     pincode : 402144,
-//     itemid: 2,
-//     itemQuantity: 10,
-//     address:"blablablabstreetd 20031"
-//   }
-// ]
